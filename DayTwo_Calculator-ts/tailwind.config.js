34a39df69e6e443b14331cbb4c14e4460d92a32b
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-02 09:01:18 +0200

    parcel + css index

--- DayTwo_Calculator-ts/tailwind.config.js
+++ DayTwo_Calculator-ts/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./src/**/*.ts", "./**/*.html"],
   theme: {
     extend: {},
   },
