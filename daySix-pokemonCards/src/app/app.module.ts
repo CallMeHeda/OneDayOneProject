d87430888eb46bf2c49bdb2c803dfb19176fe937
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-06 10:27:02 +0200

    swiper js not working with ng for

--- daySix-pokemonCards/src/app/app.module.ts
+++ daySix-pokemonCards/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { SwiperModule } from 'swiper/angular';
+
+// import { register } from 'swiper/element/bundle';
+// register();
+
 @NgModule({
-  declarations: [AppComponent, PokemonComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    PokemonComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, SwiperModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- daySix-pokemonCards/src/app/components/pokemon/pokemon.component.ts
+++ daySix-pokemonCards/src/app/components/pokemon/pokemon.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchDataService } from 'src/app/services/fetchData/fetch-data.service';
+// swiper bundle styles
+import 'swiper/css/bundle';
+
+// swiper core styles
+import 'swiper/css';
 
 @Component({
   selector: 'app-pokemon',
@@ -7,9 +12,36 @@ import { FetchDataService } from 'src/app/services/fetchData/fetch-data.service'
   styleUrls: ['./pokemon.component.scss'],
 })
 export class PokemonComponent implements OnInit {
+  cards: any[] = [];
+
   constructor(private fetchDataService: FetchDataService) {}
 
-  ngOnInit(): void {
-    this.fetchDataService.fetchCardsData();
+  ngOnInit() {
+    this.fetchDataService.fetchCardsData().then((cards: any[]) => {
+      this.cards = cards;
+
+      // this.config = {
+      //   autoHeight: true,
+      //   spaceBetween: 20,
+      //   navigation: false,
+      //   pagination: { clickable: true, dynamicBullets: true },
+      //   slidesPerView: 1,
+      //   centeredSlides: true,
+      //   breakpoints: {
+      //     400: {
+      //       slidesPerView: 'auto',
+      //       centeredSlides: false,
+      //     },
+      //   },
+      // };
+    });
+
+    // new Swiper('.mySwiper', {
+    //   effect: 'cards',
+    //   grabCursor: true,
+    //   slidesPerView: 1.5,
+    //   centeredSlides: true,
+    //   loop: true,
+    // });
   }
 }
