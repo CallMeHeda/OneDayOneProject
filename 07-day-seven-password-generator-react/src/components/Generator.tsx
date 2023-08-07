0da2b7449042f6fbafca2be14315aab741ec072d
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-07 11:55:47 +0200

    clipboard + previews

--- 07-day-seven-password-generator-react/src/components/Generator.tsx
+++ 07-day-seven-password-generator-react/src/components/Generator.tsx
@@ -1,8 +1,11 @@
 import { useState, ChangeEvent, MouseEvent } from "react";
+import { FaCopy } from "react-icons/fa";
+import { FaKey } from "react-icons/fa";
 
 export default function Generator() {
   const [password, setPassword] = useState<string>("");
   const [rangeValue, setRangeValue] = useState<number>(8);
+  const [passwordCopied, setPasswordCopied] = useState<string>("");
 
   const characters: string[] = [];
 
@@ -28,9 +31,19 @@ export default function Generator() {
     generatePassword();
   };
 
+  const handleCopyPassword = () => {
+    if (password) {
+      navigator.clipboard.writeText(password);
+      setPasswordCopied("Password copied");
+    }
+  };
+
   return (
     <div className="generator flex justify-center">
       <form className="m-6 lg:m-12 lg:w-1/3">
+        <div className="flex justify-end items-center uppercase mb-2">
+          <span className="text-xs">{passwordCopied}</span>
+        </div>
         {/* INPUT PASSWORD */}
         <div className="flex">
           <input
@@ -38,9 +51,14 @@ export default function Generator() {
             name="password"
             id="password"
             value={password}
-            readOnly
             className="flex w-full bg-gray-800 text-white py-2 px-3 mb-2 focus:outline-none"
+            readOnly
           />
+          <div className="relative">
+            <span className="flex justify-center h-5/6 items-center absolute inset-y-0 right-2">
+              <FaCopy onClick={handleCopyPassword} />
+            </span>
+          </div>
         </div>
         {/* INPUT RANGE */}
         <div className="flex justify-center items-center">
@@ -54,16 +72,14 @@ export default function Generator() {
             onChange={handleChange}
             className="w-full mr-2 accent-rose-700"
           />
-          <span className="range flex font-serif font-medium">
-            {rangeValue}
-          </span>
+          <span className="flex font-serif font-medium">{rangeValue}</span>
         </div>
         <div className="flex justify-center mt-5">
           <button
-            className="btn flex items-center p-2 rounded bg-slate-700 shadow-md shadow-black text-slate-200 capitalize"
+            className="flex items-center p-2 rounded bg-slate-700 shadow-md shadow-black text-slate-200 capitalize"
             onClick={handleClick}
           >
-            Generate password
+            Generate password <FaKey className="ml-2 text-rose-500" />
           </button>
         </div>
       </form>
