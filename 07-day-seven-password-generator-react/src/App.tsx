a637aa045923743f5297759b9b2fad87d37ef32f
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-07 11:15:12 +0200

    tailwind

--- 07-day-seven-password-generator-react/src/App.tsx
+++ 07-day-seven-password-generator-react/src/App.tsx
@@ -4,7 +4,17 @@ import Generator from "./components/Generator";
 function App() {
   return (
     <div className="app">
+      <h1 className="text-center mt-7 font-serif uppercase lg:text-xl lg:mt-10">
+        <span className="text-rose-700">DAY 7 :</span> Password Generator
+        <span className="text-rose-700">.</span>
+      </h1>
       <Generator />
+      <footer className="flex justify-center fixed bottom-0 left-0 p-4 w-full">
+        <p className="text-sm">
+          © 2023 - <span className="text-rose-700">Fatima.</span> Tous droits
+          réservés.
+        </p>
+      </footer>
     </div>
   );
 }
--- 07-day-seven-password-generator-react/src/components/Generator.tsx
+++ 07-day-seven-password-generator-react/src/components/Generator.tsx
@@ -29,20 +29,21 @@ export default function Generator() {
   };
 
   return (
-    <div className="generator">
-      <form className="form">
+    <div className="generator flex justify-center">
+      <form className="m-6 lg:m-12 lg:w-1/3">
         {/* INPUT PASSWORD */}
-        <div>
+        <div className="flex">
           <input
             type="text"
             name="password"
             id="password"
             value={password}
             readOnly
+            className="flex w-full bg-gray-800 text-white py-2 px-3 mb-2 focus:outline-none"
           />
         </div>
         {/* INPUT RANGE */}
-        <div>
+        <div className="flex justify-center items-center">
           <input
             type="range"
             name="length"
@@ -51,11 +52,17 @@ export default function Generator() {
             max={25}
             value={rangeValue}
             onChange={handleChange}
+            className="w-full mr-2 accent-rose-700"
           />
-          <span className="range">{rangeValue}</span>
+          <span className="range flex font-serif font-medium">
+            {rangeValue}
+          </span>
         </div>
-        <div className="buttonBox">
-          <button className="btn" onClick={handleClick}>
+        <div className="flex justify-center mt-5">
+          <button
+            className="btn flex items-center p-2 rounded bg-slate-700 shadow-md shadow-black text-slate-200 capitalize"
+            onClick={handleClick}
+          >
             Generate password
           </button>
         </div>
