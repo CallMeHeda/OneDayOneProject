851bfd3f6622924a907f9d6862bdf0f3908931bc
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-10 14:50:24 +0200

    details page

--- 10-day-ten-tv-shows-angular/frontend/src/app/app.module.ts
+++ 10-day-ten-tv-shows-angular/frontend/src/app/app.module.ts
@@ -6,18 +6,27 @@ import { AppComponent } from './app.component';
 
 import { TvShowsComponent } from './components/tv-shows/tv-shows.component';
 import { CardSeriesComponent } from './components/card-series/card-series.component';
+import { TvShowsDetailsComponent } from './components/tv-shows-details/tv-shows-details.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-import { TvShowsDetailsComponent } from './components/tv-shows-details/tv-shows-details.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [AppComponent, TvShowsComponent, CardSeriesComponent, TvShowsDetailsComponent],
+  declarations: [
+    AppComponent,
+    TvShowsComponent,
+    CardSeriesComponent,
+    TvShowsDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatProgressSpinnerModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- 10-day-ten-tv-shows-angular/frontend/src/app/components/tv-shows-details/tv-shows-details.component.ts
+++ 10-day-ten-tv-shows-angular/frontend/src/app/components/tv-shows-details/tv-shows-details.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import axios from 'axios';
+import { ITvShows } from 'src/app/interfaces/itv-shows';
+
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-tv-shows-details',
@@ -9,6 +13,16 @@ import axios from 'axios';
 })
 export class TvShowsDetailsComponent implements OnInit {
   id: string = '';
+  tvShowsDetails: ITvShows = {
+    id: 0,
+    name: '',
+  };
+  value: number = 0;
+
+  color: ThemePalette = 'primary';
+  mode: ProgressSpinnerMode = 'determinate';
+  customDiameter: number = 68;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -26,7 +40,40 @@ export class TvShowsDetailsComponent implements OnInit {
       const response = await axios.request(options);
       const series = response.data;
 
-      console.log(series);
+      this.tvShowsDetails = {
+        id: series.id,
+        name: series.name,
+        first_air_date: series.first_air_date,
+        last_air_date: series.last_air_date,
+        overview: series.overview,
+        poster_path: 'https://image.tmdb.org/t/p/original' + series.poster_path,
+        backdrop_path:
+          'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces' +
+          series.backdrop_path,
+        vote_average: series.vote_average,
+        created_by: series.created_by,
+        genres: series.genres,
+        in_production: series.in_production,
+        networks: [
+          {
+            id: series.networks[0].id,
+            logo_path:
+              'https://image.tmdb.org/t/p/original' +
+              series.networks[0].logo_path,
+            name: series.networks[0].name,
+          },
+        ],
+        number_of_episodes: series.number_of_episodes,
+        number_of_seasons: series.number_of_seasons,
+        production_countries: series.production_countries,
+        status: series.status,
+        tagline: series.tagline,
+      };
+
+      if (this.tvShowsDetails.vote_average) {
+        const rating = (this.tvShowsDetails.vote_average / 10) * 100;
+        this.value = Math.round(rating);
+      }
     } catch (error) {
       console.log(error);
       throw error;
