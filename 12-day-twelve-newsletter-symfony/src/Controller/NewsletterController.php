4b9110d1fdacbc54e7d420c40f1c3d75fc935946
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-12 11:49:06 +0200

    send email register

--- 12-day-twelve-newsletter-symfony/src/Controller/NewsletterController.php
+++ 12-day-twelve-newsletter-symfony/src/Controller/NewsletterController.php
@@ -4,20 +4,66 @@ namespace App\Controller;
 
 use App\Entity\Newsletter\User;
 use App\Form\NewsletterUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsletterController extends AbstractController
 {
     #[Route('/newsletter', name: 'app_newsletter')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, MailerInterface $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(NewsletterUserType::class, $user);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $token = hash('sha256', uniqid());
+            $user->setValidationToken($token);
+            $user->setCreatedAt(new \DateTimeImmutable());
+
+            $em->persist($user);
+            $em->flush();
+
+            $email = (new TemplatedEmail())
+                ->from('newsletter@fatima.be')
+                ->to($user->getEmail())
+                ->subject('Newsletter subscription')
+                ->htmlTemplate('emails/subscription.html.twig')
+                ->context(compact('user', 'token'));
+            // ->context(['user' => $user, 'token' => $token]);
+
+            $mailer->send($email);
+
+            $this->addFlash('message', 'Registration pending validation');
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('newsletter/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/newsletter/confirm/{id}/{token}', name: 'confirm')]
+    public function confirm(User $user, $token, EntityManagerInterface $em): Response
+    {
+        if ($user->getValidationToken() != $token) {
+            throw $this->createNotFoundException('Not Found');
+        }
+
+        $user->setIsValid(true);
+
+        $em->persist($user);
+        $em->flush();
+
+        $this->addFlash('message', 'Confirmed !');
+
+        return $this->redirectToRoute('app_home');
     }
 }
