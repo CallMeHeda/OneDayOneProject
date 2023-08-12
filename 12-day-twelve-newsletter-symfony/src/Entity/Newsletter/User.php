87c2dcd50fe95a978c43e474e21a936b86996a68
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-12 09:47:00 +0200

    entity User

---
+++ 12-day-twelve-newsletter-symfony/src/Entity/Newsletter/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity\Newsletter;
+
+use App\Repository\Newsletter\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity('email', message: "This email is already in use.")]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?bool $is_rgpd = false;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $validation_token = null;
+
+    #[ORM\Column]
+    private ?bool $is_valid = false;
+
+    public function __construct(){}
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function isIsRgpd(): ?bool
+    {
+        return $this->is_rgpd;
+    }
+
+    public function setIsRgpd(bool $is_rgpd): static
+    {
+        $this->is_rgpd = $is_rgpd;
+
+        return $this;
+    }
+
+    public function getValidationToken(): ?string
+    {
+        return $this->validation_token;
+    }
+
+    public function setValidationToken(string $validation_token): static
+    {
+        $this->validation_token = $validation_token;
+
+        return $this;
+    }
+
+    public function getIsValid(): ?bool
+    {
+        return $this->is_valid;
+    }
+
+    public function setIsValid(bool $is_valid): static
+    {
+        $this->is_valid = $is_valid;
+
+        return $this;
+    }
+
+}
