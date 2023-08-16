936da0f536be44815006345f6a4bc09aa61f26d5
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-16 10:16:10 +0200

    utils heroes

---
+++ 16-day-sixteen-MemoryGame-react-native/app/utils/heroes.js
@@ -0,0 +1,317 @@
+export const damages = [
+  {
+    name: "Ashe",
+    image: require("../../assets/images/damage/ashe.png"),
+  },
+  {
+    name: "Bastion",
+    image: require("../../assets/images/damage/bastion.png"),
+  },
+  {
+    name: "Cassidy",
+    image: require("../../assets/images/damage/cassidy.png"),
+  },
+  {
+    name: "Chacal",
+    image: require("../../assets/images/damage/chacal.png"),
+  },
+  {
+    name: "Echo",
+    image: require("../../assets/images/damage/echo.png"),
+  },
+  {
+    name: "Fatale",
+    image: require("../../assets/images/damage/fatale.png"),
+  },
+  {
+    name: "Genji",
+    image: require("../../assets/images/damage/genji.png"),
+  },
+  {
+    name: "Hanzo",
+    image: require("../../assets/images/damage/hanzo.png"),
+  },
+  {
+    name: "Mei",
+    image: require("../../assets/images/damage/mei.png"),
+  },
+  {
+    name: "Pharah",
+    image: require("../../assets/images/damage/pharah.png"),
+  },
+  {
+    name: "Reaper",
+    image: require("../../assets/images/damage/reaper.png"),
+  },
+  {
+    name: "Sojourn",
+    image: require("../../assets/images/damage/sojourn.png"),
+  },
+  {
+    name: "Soldat: 76",
+    image: require("../../assets/images/damage/soldat76.png"),
+  },
+  {
+    name: "Sombra",
+    image: require("../../assets/images/damage/sombra.png"),
+  },
+  {
+    name: "Symmetra",
+    image: require("../../assets/images/damage/symmetra.png"),
+  },
+  {
+    name: "Torbjorn",
+    image: require("../../assets/images/damage/torbjorn.png"),
+  },
+  {
+    name: "Tracer",
+    image: require("../../assets/images/damage/tracer.png"),
+  },
+]; // 17 Images
+
+export const supports = [
+  {
+    name: "Ana",
+    image: require("../../assets/images/support/ana.png"),
+  },
+  {
+    name: "Baptiste",
+    image: require("../../assets/images/support/baptiste.png"),
+  },
+  {
+    name: "Brigitte",
+    image: require("../../assets/images/support/brigitte.png"),
+  },
+  {
+    name: "Illari",
+    image: require("../../assets/images/support/illari.png"),
+  },
+  {
+    name: "Kiriko",
+    image: require("../../assets/images/support/kiriko.png"),
+  },
+  {
+    name: "Lùcio",
+    image: require("../../assets/images/support/lùcio.png"),
+  },
+  {
+    name: "Mercy",
+    image: require("../../assets/images/support/mercy.png"),
+  },
+  {
+    name: "Moira",
+    image: require("../../assets/images/support/moira.png"),
+  },
+  {
+    name: "Vital",
+    image: require("../../assets/images/support/vital.png"),
+  },
+  {
+    name: "Zenyatta",
+    image: require("../../assets/images/support/zenyatta.png"),
+  },
+]; // 9 images
+
+export const tanks = [
+  {
+    name: "Bouldozer",
+    image: require("../../assets/images/tank/bouldozer.png"),
+  },
+  {
+    name: "Chopper",
+    image: require("../../assets/images/tank/chopper.png"),
+  },
+  {
+    name: "Doomfist",
+    image: require("../../assets/images/tank/doomfist.png"),
+  },
+  {
+    name: "D.VA",
+    image: require("../../assets/images/tank/DVA.png"),
+  },
+  {
+    name: "Junker Queen",
+    image: require("../../assets/images/tank/junkerQueen.png"),
+  },
+  {
+    name: "Orisa",
+    image: require("../../assets/images/tank/orisa.png"),
+  },
+  {
+    name: "Ramattra",
+    image: require("../../assets/images/tank/ramattra.png"),
+  },
+  {
+    name: "Reinhardt",
+    image: require("../../assets/images/tank/reinhardt.png"),
+  },
+  {
+    name: "Sigma",
+    image: require("../../assets/images/tank/sigma.png"),
+  },
+  {
+    name: "Winston",
+    image: require("../../assets/images/tank/winston.png"),
+  },
+  {
+    name: "Zarya",
+    image: require("../../assets/images/tank/zarya.png"),
+  },
+]; // 11 images
+
+export const all = [
+  // DAMAGES
+  {
+    name: "Ashe",
+    image: require("../../assets/images/damage/ashe.png"),
+  },
+  {
+    name: "Bastion",
+    image: require("../../assets/images/damage/bastion.png"),
+  },
+  {
+    name: "Cassidy",
+    image: require("../../assets/images/damage/cassidy.png"),
+  },
+  {
+    name: "Chacal",
+    image: require("../../assets/images/damage/chacal.png"),
+  },
+  {
+    name: "Echo",
+    image: require("../../assets/images/damage/echo.png"),
+  },
+  {
+    name: "Fatale",
+    image: require("../../assets/images/damage/fatale.png"),
+  },
+  {
+    name: "Genji",
+    image: require("../../assets/images/damage/genji.png"),
+  },
+  {
+    name: "Hanzo",
+    image: require("../../assets/images/damage/hanzo.png"),
+  },
+  {
+    name: "Mei",
+    image: require("../../assets/images/damage/mei.png"),
+  },
+  {
+    name: "Pharah",
+    image: require("../../assets/images/damage/pharah.png"),
+  },
+  {
+    name: "Reaper",
+    image: require("../../assets/images/damage/reaper.png"),
+  },
+  {
+    name: "Sojourn",
+    image: require("../../assets/images/damage/sojourn.png"),
+  },
+  {
+    name: "Soldat: 76",
+    image: require("../../assets/images/damage/soldat76.png"),
+  },
+  {
+    name: "Sombra",
+    image: require("../../assets/images/damage/sombra.png"),
+  },
+  {
+    name: "Symmetra",
+    image: require("../../assets/images/damage/symmetra.png"),
+  },
+  {
+    name: "Torbjorn",
+    image: require("../../assets/images/damage/torbjorn.png"),
+  },
+  {
+    name: "Tracer",
+    image: require("../../assets/images/damage/tracer.png"),
+  },
+  // SUPPORTS
+  {
+    name: "Ana",
+    image: require("../../assets/images/support/ana.png"),
+  },
+  {
+    name: "Baptiste",
+    image: require("../../assets/images/support/baptiste.png"),
+  },
+  {
+    name: "Brigitte",
+    image: require("../../assets/images/support/brigitte.png"),
+  },
+  {
+    name: "Illari",
+    image: require("../../assets/images/support/illari.png"),
+  },
+  {
+    name: "Kiriko",
+    image: require("../../assets/images/support/kiriko.png"),
+  },
+  {
+    name: "Lùcio",
+    image: require("../../assets/images/support/lùcio.png"),
+  },
+  {
+    name: "Mercy",
+    image: require("../../assets/images/support/mercy.png"),
+  },
+  {
+    name: "Moira",
+    image: require("../../assets/images/support/moira.png"),
+  },
+  {
+    name: "Vital",
+    image: require("../../assets/images/support/vital.png"),
+  },
+  {
+    name: "Zenyatta",
+    image: require("../../assets/images/support/zenyatta.png"),
+  },
+  {
+    name: "Bouldozer",
+    image: require("../../assets/images/tank/bouldozer.png"),
+  },
+  {
+    name: "Chopper",
+    image: require("../../assets/images/tank/chopper.png"),
+  },
+  {
+    name: "Doomfist",
+    image: require("../../assets/images/tank/doomfist.png"),
+  },
+  {
+    name: "D.VA",
+    image: require("../../assets/images/tank/DVA.png"),
+  },
+  {
+    name: "Junker Queen",
+    image: require("../../assets/images/tank/junkerQueen.png"),
+  },
+  {
+    name: "Orisa",
+    image: require("../../assets/images/tank/orisa.png"),
+  },
+  {
+    name: "Ramattra",
+    image: require("../../assets/images/tank/ramattra.png"),
+  },
+  {
+    name: "Reinhardt",
+    image: require("../../assets/images/tank/reinhardt.png"),
+  },
+  {
+    name: "Sigma",
+    image: require("../../assets/images/tank/sigma.png"),
+  },
+  {
+    name: "Winston",
+    image: require("../../assets/images/tank/winston.png"),
+  },
+  {
+    name: "Zarya",
+    image: require("../../assets/images/tank/zarya.png"),
+  },
+]
