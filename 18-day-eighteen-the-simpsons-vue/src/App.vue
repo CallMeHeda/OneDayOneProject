5bf4824fb411a3f7922d7935deeddab82d512897
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-18 15:49:04 +0200

    footer

--- 18-day-eighteen-the-simpsons-vue/src/App.vue
+++ 18-day-eighteen-the-simpsons-vue/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Footer from './components/Footer.vue'
+</script>
 
 <template>
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ 18-day-eighteen-the-simpsons-vue/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p><span>Day 18</span> | © 2023 - <span>Fatima.</span> Tous droits réservés.</p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+footer {
+  @apply flex justify-center fixed bottom-0 w-full p-1;
+}
+p {
+  @apply text-lg text-gray-800 mb-0 font-cursive;
+}
+span {
+  @apply text-red-700;
+}
+</style>
