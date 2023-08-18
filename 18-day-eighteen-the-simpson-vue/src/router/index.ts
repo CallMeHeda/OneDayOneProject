89953349d1c2f66ea422228c2ea24a0a65981f9a
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-18 09:08:43 +0200

    fetch data

--- 18-day-eighteen-the-simpson-vue/src/router/index.ts
+++ 18-day-eighteen-the-simpson-vue/src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'simpson',
+      component: () => import('../views/SimpsonView.vue')
+    }
+  ]
 })
 
 export default router
---
+++ 18-day-eighteen-the-simpson-vue/src/views/SimpsonView.vue
@@ -0,0 +1,9 @@
+<template>
+  <Simpson />
+</template>
+
+<script setup lang="ts">
+import Simpson from '@/components/Simpson.vue'
+</script>
+
+<style scoped></style>
