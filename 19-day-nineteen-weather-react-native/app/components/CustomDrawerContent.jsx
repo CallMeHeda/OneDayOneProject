d5a8e4465a4d23ba2f3ee7d60d48d9183074f43e
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-20 00:02:50 +0200

    css navigation drawer

---
+++ 19-day-nineteen-weather-react-native/app/components/CustomDrawerContent.jsx
@@ -0,0 +1,32 @@
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import LottieView from "lottie-react-native";
+import { View } from "react-native";
+
+export default CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          paddingVertical: 20,
+          backgroundColor: "transparent",
+        }}
+      >
+        <LottieView
+          autoPlay
+          style={{
+            width: 500,
+            height: 500,
+            alignSelf: "center",
+          }}
+          source={require("../../assets/navigation/animationNav.json")}
+        />
+      </View>
+    </DrawerContentScrollView>
+  );
+};
--- 19-day-nineteen-weather-react-native/app/components/Navigation.jsx
+++ 19-day-nineteen-weather-react-native/app/components/Navigation.jsx
@@ -1,13 +1,53 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import WeatherGeolocation from "./WeatherGeolocation";
 import WeatherByCity from "./WeatherByCity";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import CustomDrawerContent from "./CustomDrawerContent";
 
 const Drawer = createDrawerNavigator();
+
 export default function Navigation() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Localisation" component={WeatherGeolocation} />
-      <Drawer.Screen name="Search" component={WeatherByCity} />
+    <Drawer.Navigator
+      initialRouteName="Location"
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: "#cd3545",
+        drawerActiveTintColor: "#fff",
+        drawerInactiveTintColor: "#333",
+        drawerLabelStyle: {
+          marginLeft: -25,
+          fontSize: 18,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Location"
+        component={WeatherGeolocation}
+        options={{
+          drawerIcon: ({ focused, color }) => (
+            <Ionicons
+              name="location"
+              size={30}
+              color={focused ? color : "#333"}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Search"
+        component={WeatherByCity}
+        options={{
+          drawerIcon: ({ focused, color }) => (
+            <Ionicons
+              name="search"
+              size={30}
+              color={focused ? color : "#333"}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
