4b40b985edf7c5fc8e741aa63edf00142909a3dc
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-21 11:54:40 +0200

    Animal model

---
+++ 20-day-twenty-one-my-api-node/Backend/models/animal.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const animalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  taxonomy: {
+    type: [String],
+  },
+  habitat: {
+    type: String,
+    required: true,
+  },
+  diet: {
+    type: String,
+    required: true,
+  },
+  characteristics: {
+    type: String,
+  },
+  behavior: {
+    type: String,
+  },
+  conservation_status: {
+    type: String,
+    required: true,
+  },
+  fun_facts: {
+    type: [String],
+    required: true,
+  },
+  image: {
+    type: String,
+    format: "uri",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Animal", animalSchema);
