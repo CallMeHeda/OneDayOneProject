77204ba2b9244902f0b952c279f715d4e7e6c73e
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-24 11:33:41 +0200

    form

--- 23-translator-react/src/components/Translator.tsx
+++ 23-translator-react/src/components/Translator.tsx
@@ -1,10 +1,15 @@
 import axios from "axios";
-import { useState, ChangeEvent } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 
 export default function Translator() {
-  const [textToTranslate, setTextToTranslate] = useState("Bonjour");
-  const [language, setLanguage] = useState("en");
+  const [textToTranslate, setTextToTranslate] = useState("");
+  const [languageTarget, setLanguageTarget] = useState("en");
   const [translatedText, setTranslatedText] = useState("");
+  const [languages, setLanguages] = useState([{ language: "", name: "" }]);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const handleTranslate = async (e: any) => {
     e.preventDefault();
@@ -13,7 +18,8 @@ export default function Translator() {
       url: "https://api-free.deepl.com/v2/translate",
       params: {
         text: textToTranslate,
-        target_lang: language,
+        source_lang: languages,
+        target_lang: languageTarget,
         auth_key: process.env.REACT_APP_API_KEY,
       },
     };
@@ -27,8 +33,7 @@ export default function Translator() {
     }
   };
 
-  const getData = async (e: any) => {
-    e.preventDefault();
+  const getData = async () => {
     const options = {
       method: "GET",
       url: "https://api-free.deepl.com/v2/languages",
@@ -39,23 +44,59 @@ export default function Translator() {
 
     try {
       const response = await axios.request(options);
-      console.log(response.data);
+      setLanguages(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+
+    if (name === "textToTranslate") {
+      setTextToTranslate(value);
+    } else if (name === "language") {
+      setLanguageTarget(value);
+    }
+  };
+
   return (
     <div>
-      <button type="submit" onClick={handleTranslate}>
-        Translate
-      </button>
+      <h1>Translation</h1>
+      <form onSubmit={handleTranslate}>
+        <label htmlFor="textToTranslate">Text to Translate:</label>
+        <input
+          type="text"
+          name="textToTranslate"
+          id="textToTranslate"
+          value={textToTranslate}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <label htmlFor="language">Target Language:</label>
+        <select
+          id="language"
+          value={languageTarget}
+          name="language"
+          onChange={handleInputChange}
+          required
+        >
+          {languages.map((lang, index) => (
+            <option key={index} value={lang.language}>
+              {lang.name}
+            </option>
+          ))}
+        </select>
+        <br />
+        <button type="submit">Translate</button>
+      </form>
       <div>
         <h2>Translated Text:</h2>
         <p>{translatedText}</p>
       </div>
-
-      <button onClick={getData}>CLICK</button>
     </div>
   );
 }
