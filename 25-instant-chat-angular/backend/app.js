ecec7a993a4009094e73215e069092c29544fd3b
FatimaElamri <fatimaelamri789@gmail.com>
2023-08-26 12:29:07 +0200

    socket

---
+++ 25-instant-chat-angular/backend/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 3000;
+
+// app.get("/", (req, res) => {});
+
+io.on("connection", (socket) => {
+  // when the client emits 'user joined', this listens and executes
+  socket.on("join", (data) => {
+    console.log(data.friendsId);
+    socket.join(data.friendsId);
+    // we tell the client to execute 'user joined'
+    socket.broadcast.to(data.friendsId).emit("user joined");
+  });
+
+  socket.on("message", (data) => {
+    // io.in & io.to are the same (From what I understand from the documentation)
+    io.to(data.friendsId).emit("new message", {
+      user: data.user,
+      message: data.message,
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *: ${port}`);
+});
